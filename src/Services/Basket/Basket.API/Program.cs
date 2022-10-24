16c451d135806ae777bcbbbc2e807042ff5e98e8
StefanStrauberg <stefanstrauberg@gmail.com>
2022-10-24 21:34:38 +0300

    Basket.API

---
+++ src/Services/Basket/Basket.API/Program.cs
@@ -0,0 +1,21 @@
+using Basket.API.Repositories;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    options.Configuration = builder.Configuration.GetValue<string>("CacheSettings:ConnectionString");
+});
+builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.MapControllers();
+
+app.Run();
