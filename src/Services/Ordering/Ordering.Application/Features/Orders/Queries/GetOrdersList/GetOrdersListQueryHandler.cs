a568b09a7e343c9efc9b4785c4c8074f201eeede
StefanStrauberg <StefanStrauberg>
2022-11-16 01:52:36 +0300

    Order Service Initialize

---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Queries/GetOrdersList/GetOrdersListQueryHandler.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using MediatR;
+using Ordering.Application.Contracts.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace Ordering.Application.Features.Orders.Queries.GetOrdersList
+{
+    internal class GetOrdersListQueryHandler : IRequestHandler<GetOrdersListQuery, List<OrderVm>>
+    {
+        readonly IOrderRepository _repository;
+        readonly IMapper _mapper;
+        public GetOrdersListQueryHandler(IMapper mapper, IOrderRepository repository)
+        {
+            _mapper = mapper ?? throw new ArgumentException(nameof(mapper));
+            _repository = repository ?? throw new ArgumentException(nameof(repository));
+        }
+        public async System.Threading.Tasks.Task<List<OrderVm>> Handle(GetOrdersListQuery request, CancellationToken cancellationToken)
+        {
+            var orderList = await _repository.GetOrdersByUserName(request.UserName);
+            return _mapper.Map<List<OrderVm>>(orderList);
+        }
+    }
+}
