3b0a04ef6f15654bf6e305ef627d71bd4ec512b8
StefanStrauberg <StefanStrauberg@gmail.com>
2022-12-01 23:26:12 +0300

    Adding Add Interfaces, Add HttpClient Extension, Add implementation CatalogService, Add DI Services

--- src/ApiGateways/Shopping.Aggregator/Program.cs
+++ src/ApiGateways/Shopping.Aggregator/Program.cs
@@ -1,9 +1,23 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Shopping.Aggregator.Services;
+using System;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddHttpClient<ICatalogService, CatalogService>(client =>
+{
+    client.BaseAddress = new Uri(builder.Configuration["ApiSettings:CatalogUrl"]);
+});
+builder.Services.AddHttpClient<IBasketService, BasketService>(client =>
+{
+    client.BaseAddress = new Uri(builder.Configuration["ApiSettings:BasketUrl"]);
+});
+builder.Services.AddHttpClient<IOrderService, OrderService>(client =>
+{
+    client.BaseAddress = new Uri(builder.Configuration["ApiSettings:OrderingUrl"]);
+});
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
